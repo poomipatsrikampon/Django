3718f1abc66c86c63bfa1fee9e0326ed3cfb0465
poomipatsrikampon <48234384+poomipatsrikampon@users.noreply.github.com>
2022-05-16 20:02:45 +0700

    day 2

--- Django50hr/firstweb/myapp/models.py
+++ Django50hr/firstweb/myapp/models.py
@@ -5,7 +5,10 @@ class allProduct(models.Model):
     price = models.IntegerField()
     detail = models.TextField(null=True, blank=True)
     image_url = models.CharField(max_length=200, null=True, blank=True)
-
+    instock = models.BooleanField(default=True)
+    image = models.ImageField(upload_to="products", null=True, blank=True)
+    unit = models.CharField(max_length=200,default="-")
+    quantity = models.IntegerField(default=1)
 
     def __str__(self):
         return self.name
--- Django50hr/firstweb/myapp/views.py
+++ Django50hr/firstweb/myapp/views.py
@@ -2,6 +2,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import *
+# import upload FILE
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 def home_view(request):
@@ -19,19 +21,33 @@ def contract(request):
 
 def add_product(request):
     
-    if request.method =='POST':
+    if request.method =='POST' and request.FILES['imageupload']:
 
         data = request.POST.copy()
         name = data.get('name')
         price = data.get('price')
         image_url = data.get('imageurl')
         detail = data.get('detail')
+        quantity = data.get('quantity')
+        unit = data.get('unit')
 
         new = allProduct()
         new.name = name
         new.price = price
         new.image_url =image_url
         new.detail = detail
+        new.quantity = quantity
+        new.unit = unit
+
+        #upload image------------
+        file_image = request.FILES['imageupload']
+        file_image_name = request.FILES['imageupload'].name.replace(' ','')
+        fs = FileSystemStorage()
+        filename =fs.save(file_image_name,file_image)
+        upload_file_url = fs.url(filename)
+
+        new.image = upload_file_url[6:]
+        #------------------------
         new.save()
 
     
@@ -40,9 +56,11 @@ def add_product(request):
 
 def product(request):
 
-    product = allProduct.objects.all()
+    product = allProduct.objects.all().order_by('id').reverse()
     context = {
         'product':product
     }
+
     
     return render(request,'myapp/allproduct.html',context)
+
